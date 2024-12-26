697464f96a00b86fa15cbf244933d742ee903ac1
ptrodion <rdnpetrenko@gmail.com>
2024-12-26 19:32:59 +0000

    Added дщпшс ащк 404 pages and added Suspense

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom"
+import { Suspense } from "react"
+import { Navigate, Route, Routes } from "react-router-dom"
 
 
 const MainRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={"Home Page"} />
-      <Route path="/vehicles" element={"Vehicles Page"} />
-      <Route path="/vehicles/:id" element={"Vehicles id Page"} />
-    </Routes>
+    <Suspense fallback={<div> Loading.....</div>}>
+      <Routes>
+        <Route path="/" element={"Home Page"} />
+        <Route path="/vehicles" element={"Vehicles Page"} />
+        <Route path="/vehicles/:id" element={"Vehicles id Page"} />
+        <Route path="/not-found" element={"Not Found"} />
+        <Route path="*" element={<Navigate replace to="/not-found" />} />
+      </Routes>
+    </Suspense>
   )
 }
 
